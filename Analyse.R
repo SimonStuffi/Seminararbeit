54a79e3bf3690d6d9879a76a29a3054cf0f72457
SimonStuffi <simon.berga@uni-ulm.de>
2024-05-28 14:12:57 +0200

    heyho

--- Analyse.R
+++ Analyse.R
@@ -6,16 +6,33 @@ library(stargazer)
 library(AER)
 library(scales)
 library(readxl)
+library(caTools)
+library(rpart)
+library(rpart.plot)
 
 
 kunden_2016 <- read_excel("Kunden 2016.xlsx") %>%
   mutate(Jahr = as.numeric(2016))
+
 kunden_2017 <- read_excel("Kunden 2017.xlsx") %>%
-  mutate(Jahr = as.numeric(2016))
+  mutate(Jahr = as.numeric(2017))
+prog2017 <- kunden_2017[c(2,9)]
+names(prog2017) <- c("Unternehmen","Prognose")
+kunden_2016 <- left_join(kunden_2016,prog2017,"Unternehmen")
+ 
 kunden_2018 <- read_excel("Kunden 2018.xlsx") %>%
-  mutate(Jahr = as.numeric(2016))
+  mutate(Jahr = as.numeric(2018))
+prog2018 <- kunden_2018[c(2,9)]
+names(prog2018) <- c("Unternehmen","Prognose")
+kunden_2017 <- left_join(kunden_2017,prog2018,"Unternehmen")
+
+
 kunden_2019 <- read_excel("Kunden 2019.xlsx") %>%
-  mutate(Jahr = as.numeric(2016))
+  mutate(Jahr = as.numeric(2019))
+prog2019 <- kunden_2019[c(2,9)]
+names(prog2019) <- c("Unternehmen","Prognose")
+kunden_2018 <- left_join(kunden_2018,prog2019,"Unternehmen")
+
 plz <- read_excel("Postleitzahlen.xlsx")
 
 names(plz)[3] = "PLZ"
@@ -25,12 +42,14 @@ dplz <- plz %>% distinct(PLZ, .keep_all = TRUE)
 kunden <- bind_rows(kunden_2016, kunden_2017, kunden_2018, kunden_2019)
 ges <- left_join(kunden, dplz, "PLZ")
 
-kd <- ges[-c(1,11,12)]
-kd$Kundenklasse <- kd$Kundenklasse == "A"
+kd <- ges[-c(1,13,12)]
+'kd$Kundenklasse <- kd$Kundenklasse == "A"
 kd$Kundenklasse <- as.numeric(kd$Kundenklasse)
 
-"plot(kd$Breitengrad, kd$Längengrad)"
+plot(kd$Breitengrad, kd$Längengrad)'
 
-reg <- lm(Kundenklasse ~ Konzernmarken + Umsatz,data = kd)
+Amodel <- rpart(Prognose ~ ., data = kunden_2016, method = "class")
+plot <- rpart.plot(Amodel)
+
 
 
